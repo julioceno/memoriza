e5102153bed5bbade95cac5b43aa72452bb2f0b4
julioceno <julioreplay@gmail.com>
2025-04-25 23:35:21 -0300

    feat: improve layouft msite

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,13 +6,13 @@ import { motion } from "framer-motion";
 export default function Home() {
   return (
     <motion.div
-      className="flex h-10/12 items-center justify-center flex-col gap-7 px-4 max-w-5xl w-full mx-auto"
+      className="flex h-10/12 items-center justify-center flex-col gap-7 md:px-4 max-w-5xl w-full mx-auto"
       initial={{ opacity: 0, y: 50 }}
       animate={{ opacity: 1, y: 0 }}
       transition={{ duration: 0.8, ease: "easeOut" }}
     >
       <motion.h3
-        className="text-4xl w-full max-w-2xl font-bold text-center text-white"
+        className="text-2xl sm:text-3xl md:text-4xl w-full max-w-2xl font-bold text-center text-white"
         initial={{ opacity: 0, y: 50 }}
         animate={{ opacity: 1, y: 0 }}
         transition={{ duration: 1, ease: "easeOut", delay: 0.2 }}
@@ -20,14 +20,14 @@ export default function Home() {
         Bem-vindo ao Memoriza. Estude rápido. Lembre sempre!
       </motion.h3>
       <motion.button
-        className="relative flex items-center justify-center w-full max-w-xs px-6 py-3 bg-white text-gray-700 font-medium rounded-2xl shadow-md hover:shadow-lg cursor-pointer"
+        className="relative flex items-center justify-center w-4/5 sm:w-full max-w-xs px-6 py-3 bg-white hover:bg-gray-100 text-gray-700 font-medium rounded-2xl shadow-md hover:shadow-lg cursor-pointer"
         initial={{ opacity: 0, y: 50 }}
         animate={{ opacity: 1, y: 0 }}
         whileHover={{ y: -10 }}
         transition={{ type: "spring", stiffness: 300 }}
       >
-        <ArrowRight className="absolute left-4 w-5 h-5" strokeWidth={3} />
-        <span className="mx-auto font-bold">Vamos começar</span>
+        <ArrowRight className="absolute left-4 w-3 sm:w-5 h-3 sm:h-5" strokeWidth={3} />
+        <span className="mx-auto font-bold text-xs sm:text-sm">Vamos começar</span>
       </motion.button>
     </motion.div>
   );
