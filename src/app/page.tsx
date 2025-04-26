2b96e805c5a2e0e12d133170e181379789443a6e
julioceno <julioreplay@gmail.com>
2025-04-25 23:12:02 -0300

    feat: add emotion in project

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,34 @@
+'use client';
+
 import { ArrowRight } from "lucide-react";
+import { motion } from "framer-motion";
 
 export default function Home() {
   return (
-    <div className="flex h-10/12 items-center justify-center flex-col gap-7">
-      <h3 className="text-4xl w-5/12 font-bold text-center text-white">
+    <motion.div
+      className="flex h-10/12 items-center justify-center flex-col gap-7"
+      initial={{ opacity: 0, y: 50 }} 
+      animate={{ opacity: 1, y: 0 }}
+      transition={{ duration: 0.8, ease: "easeOut" }}
+    >
+      <motion.h3
+        className="text-4xl w-5/12 font-bold text-center text-white"
+        initial={{ opacity: 0, y: 50 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 1, ease: "easeOut", delay: 0.2 }}
+      >
         Bem-vindo ao Memoriza. Estude rápido. Lembre sempre!
-      </h3>
-      <button className="relative flex items-center justify-center w-full max-w-xs px-6 py-3 bg-white text-gray-700 font-medium rounded-2xl shadow-md hover:shadow-lg transition-transform transform hover:-translate-y-1 hover:bg-gray-100 cursor-pointer">
+      </motion.h3>
+      <motion.button
+        className="relative flex items-center justify-center w-full max-w-xs px-6 py-3 bg-white text-gray-700 font-medium rounded-2xl shadow-md hover:shadow-lg cursor-pointer"
+        initial={{ opacity: 0, y: 50 }}
+        animate={{ opacity: 1, y: 0 }}
+        whileHover={{ y: -10 }}
+        transition={{ type: "spring", stiffness: 300 }}
+      >
         <ArrowRight className="absolute left-4 w-5 h-5" strokeWidth={3} />
         <span className="mx-auto font-bold">Vamos começar</span>
-      </button>
-    </div>
+      </motion.button>
+    </motion.div>
   );
 }
