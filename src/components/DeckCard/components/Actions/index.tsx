88aa57e768df8dbfe7a5c68edd188cb03758ca49
julioceno <julioreplay@gmail.com>
2025-04-28 21:56:57 -0300

    feat: improve Msite

--- src/components/DeckCard/components/Actions/index.tsx
+++ src/components/DeckCard/components/Actions/index.tsx
@@ -2,7 +2,7 @@ import { Edit, GripVertical, Trash2 } from 'lucide-react';
 
 const Actions: React.FC = () => {
     return (
-        <div className="flex items-center gap-1.5 sm:*:">
+        <div className="flex items-center gap-1.5 sm:gap-2">
              <button className="text-gray-500 hover:text-gray-900 ">
                 <Edit className="w-5 h-5" />
             </button>
