38b03185df80d96412478d86612f35ad2508d3b2
julioceno <julioreplay@gmail.com>
2025-04-28 23:21:24 -0300

    feat: backdropClick

--- src/components/DeckCard/components/Actions/index.tsx
+++ src/components/DeckCard/components/Actions/index.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { Edit, GripVertical, Trash2 } from "lucide-react";
 import DeleteDialog from "@/components/DeleteDialog";
+import { AnimatePresence } from "framer-motion";
 
 const Actions: React.FC = () => {
   const [isDialogOpen, setIsDialogOpen] = useState(false);
@@ -12,25 +13,24 @@ const Actions: React.FC = () => {
   return (
     <>
       <div className="flex items-center gap-1.5 sm:gap-2">
-        <button className="text-gray-500 hover:text-gray-900">
+        <button className="text-gray-500 hover:text-gray-900 cursor-pointer">
           <Edit className="w-5 h-5" />
         </button>
         <button
-          className="text-gray-500 hover:text-gray-900"
+          className="text-gray-500 hover:text-gray-900 cursor-pointer"
           onClick={() => setIsDialogOpen(true)}
         >
           <Trash2 className="w-5 h-5" />
         </button>
-        <button className="text-gray-500 hover:text-gray-900">
+        <button className="text-gray-500 hover:text-gray-900 cursor-pointer">
           <GripVertical className="w-5 h-5" />
         </button>
       </div>
-      {isDialogOpen && (
-        <DeleteDialog
-          onClose={() => setIsDialogOpen(false)}
-          onConfirm={handleDelete}
-        />
-      )}
+      <DeleteDialog
+        onOpen={isDialogOpen}
+        onClose={() => setIsDialogOpen(false)}
+        onConfirm={handleDelete}
+      />
     </>
   );
 };
--- src/components/DeleteDialog/index.tsx
+++ src/components/DeleteDialog/index.tsx
@@ -1,14 +1,19 @@
 import React, { useState } from "react";
-import { motion } from "framer-motion";
+import { AnimatePresence, motion } from "framer-motion";
 import Button from "../Button";
 import Input from "../Input";
 
 interface DeleteDialogProps {
+  onOpen: boolean;
   onClose: () => void;
   onConfirm: () => void;
 }
 
-const DeleteDialog: React.FC<DeleteDialogProps> = ({ onClose, onConfirm }) => {
+const DeleteDialog: React.FC<DeleteDialogProps> = ({ 
+  onClose, 
+  onConfirm,
+  onOpen
+}) => {
   const [value, setValue] = useState("");
 
   const getStatus = () => {
@@ -17,49 +22,55 @@ const DeleteDialog: React.FC<DeleteDialogProps> = ({ onClose, onConfirm }) => {
     return "filling";
   };
 
+  const handleBackdropClick = (e: React.MouseEvent<HTMLDivElement>) => {
+    if (e.target === e.currentTarget) {
+      onClose();
+    }
+  };
+
 
   return (
-    <motion.div
-      className="fixed inset-0 flex items-center justify-center bg-black/20 z-50 px-3"
-      initial={{ opacity: 0 }}
-      animate={{ opacity: 1 }}
-      exit={{ opacity: 0 }}
-      transition={{ duration: 0.3 }}
-    >
-      <motion.div
-        className="flex flex-col items-center justify-center bg-white p-3 py-4 md:p-6 rounded-sm max-w-lg text-center"
-        initial={{ scale: 0.8, opacity: 0 }}
-        animate={{ scale: 1, opacity: 1 }}
-        exit={{ scale: 0.8, opacity: 0 }}
-        transition={{ type: "spring", stiffness: 300, damping: 20 }}
-      >
-        <h2 className="w-full text-md sm:text-xl font-bold text-gray-800 mb-4">
-          Tem certeza que deseja deletar o Deck?
-        </h2>
-        <p className="w-full text-sm sm:text-md text-gray-900">
-          Você deverá preencher o campo abaixo com a palavra{" "}
-          <span className="font-bold text-red-500">confirmar</span>, para liberar o botão de confirmação.
-        </p>
-        <div className="w-10/12 sm:w-full mt-2 mb-4">
-          <Input
-            value={value}
-            placeholder="Preenchido"
-            status={getStatus()}
-            onChange={(e) => setValue(e.target.value)}
-          />
-        </div>
-        <div className="w-30 sm:w-4/12">
+    <AnimatePresence>
+      {onOpen && (
+        <motion.div
+          className="fixed inset-0 flex items-center justify-center bg-black/20 z-50 px-3"
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          exit={{ opacity: 0 }}
+          transition={{ duration: 0.3 }}
+          onClick={handleBackdropClick}
+        >
           <motion.div
-            whileHover={{ scale: 1.05 }}
-            whileTap={{ scale: 0.95 }}
+            className="flex flex-col items-center justify-center bg-white p-3 py-4 md:p-6 rounded-sm max-w-lg text-center"
+            initial={{ scale: 0.8, opacity: 0 }}
+            animate={{ scale: 1, opacity: 1 }}
+            exit={{ scale: 0.8, opacity: 0 }}
+            transition={{ type: "spring", stiffness: 300, damping: 20 }}
           >
-            <Button full variant="rounded" size="small">
-              Deletar
-            </Button>
+            <h2 className="w-full text-md sm:text-xl font-bold text-gray-800 mb-4">
+              Tem certeza que deseja deletar o Deck?
+            </h2>
+            <p className="w-full text-sm sm:text-md text-gray-900">
+              Você deverá preencher o campo abaixo com a palavra{" "}
+              <span className="font-bold text-principal">confirmar</span>, para liberar o botão de confirmação.
+            </p>
+            <div className="w-10/12 sm:w-full mt-2 mb-4">
+              <Input
+                value={value}
+                placeholder="Insira a palavra"
+                status={getStatus()}
+                onChange={(e) => setValue(e.target.value)}
+              />
+            </div>
+            <div className="w-30 sm:w-4/12">
+                <Button full variant="rounded" size="small">
+                  Deletar
+                </Button>
+            </div>
           </motion.div>
-        </div>
-      </motion.div>
-    </motion.div>
+        </motion.div>
+      )}
+    </AnimatePresence>
   );
 };
 
