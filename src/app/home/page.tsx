efff6d4f3d7c131ae89c1dc8ec1992865cf2bf6c
julioceno <julioreplay@gmail.com>
2025-04-28 21:25:12 -0300

    feat: improve component

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -3,58 +3,33 @@ import { StatusBadgeEnum } from "@/components/DeckCard/components/StatusBadge/ty
 import SectionTitle from "@/components/SectionTitle";
 
 export default function Home() {
+  const decks = [
+    { title: "Entrevista Java", cardCount: 10, statusBadgeType: StatusBadgeEnum.SUCCESS },
+    { title: "Entrevista Java", cardCount: 10, statusBadgeType: StatusBadgeEnum.ERROR },
+    { title: "Entrevista Java", cardCount: 10, statusBadgeType: StatusBadgeEnum.SUCCESS },
+    { title: "Entrevista Java", cardCount: 10, statusBadgeType: StatusBadgeEnum.WARNING },
+    { title: "Entrevista Java", cardCount: 10, statusBadgeType: StatusBadgeEnum.SUCCESS },
+    { title: "Entrevista Java", cardCount: 10, statusBadgeType: StatusBadgeEnum.SUCCESS },
+    { title: "Entrevista Java", cardCount: 10, statusBadgeType: StatusBadgeEnum.SUCCESS },
+    { title: "Entrevista Java", cardCount: 10, statusBadgeType: StatusBadgeEnum.SUCCESS },
+    { title: "Entrevista Java", cardCount: 10, statusBadgeType: StatusBadgeEnum.SUCCESS },
+  ];
+
   return (
-      <main className="mt-20 md:mt-40">
-        <SectionTitle>
-          Aqui estao seus decks, vamos práticar!
-        </SectionTitle>
-        <div className="flex flex-wrap gap-4 mt-10 w-full max-w-7xl justify-center mx-auto">
-            <DeckCard 
-              title="Entrevista Java" 
-              cardCount={10} 
-              statusBadgeType={StatusBadgeEnum.SUCCESS}
-            />
-            <DeckCard 
-              title="Entrevista Java" 
-              cardCount={10} 
-              statusBadgeType={StatusBadgeEnum.ERROR}
-            />
-            <DeckCard 
-              title="Entrevista Java" 
-              cardCount={10} 
-              statusBadgeType={StatusBadgeEnum.SUCCESS}
-            />
-            <DeckCard 
-              title="Entrevista Java" 
-              cardCount={10} 
-              statusBadgeType={StatusBadgeEnum.WARNING}
-            />
-            <DeckCard 
-              title="Entrevista Java" 
-              cardCount={10} 
-              statusBadgeType={StatusBadgeEnum.SUCCESS}
-            />
-            <DeckCard 
-              title="Entrevista Java" 
-              cardCount={10} 
-              statusBadgeType={StatusBadgeEnum.SUCCESS}
-            />
-            <DeckCard 
-              title="Entrevista Java" 
-              cardCount={10} 
-              statusBadgeType={StatusBadgeEnum.SUCCESS}
-            />
-            <DeckCard 
-              title="Entrevista Java" 
-              cardCount={10} 
-              statusBadgeType={StatusBadgeEnum.SUCCESS}
-            />
-            <DeckCard 
-              title="Entrevista Java" 
-              cardCount={10} 
-              statusBadgeType={StatusBadgeEnum.SUCCESS}
-            />
-        </div>
-      </main>
+    <main className="mt-20 md:mt-40">
+      <SectionTitle>
+        Aqui estao seus decks, vamos práticar!
+      </SectionTitle>
+      <div className="flex flex-wrap gap-4 mt-10 w-full max-w-7xl justify-center mx-auto">
+        {decks.map((deck, index) => (
+          <DeckCard
+            key={index}
+            title={deck.title}
+            cardCount={deck.cardCount}
+            statusBadgeType={deck.statusBadgeType}
+          />
+        ))}
+      </div>
+    </main>
   );
 }
