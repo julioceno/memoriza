0cba1afb62bc905726287ab786d9b7801aeae3cb
julioceno <julioreplay@gmail.com>
2025-05-01 00:25:18 -0300

    feat: improve IconGrid

--- src/app/decks/create/IconSelection/page.tsx
+++ src/app/decks/create/IconSelection/page.tsx
@@ -4,24 +4,53 @@ import SectionTitle from '@/components/SectionTitle';
 import Button from '@/components/Button';
 import React, { useState } from 'react';
 import { Briefcase, Globe, GraduationCap } from 'lucide-react';
-import IconGrid from '@/components/IconsGrid';
+import IconGridItem from '@/components/IconGridItem/IconGrridItem';
+
+const icons = [
+    { id: 1, icon: <Briefcase />, label: "Trabalho" },
+    { id: 2, icon: <GraduationCap />, label: "Academico" },
+    { id: 3, icon: <Globe />, label: "Idiomas" },
+    { id: 4, icon: <Briefcase />, label: "Trabalho" },
+    { id: 5, icon: <GraduationCap />, label: "Academico" },
+    { id: 6, icon: <Globe />, label: "Idiomas" },
+    { id: 7, icon: <Briefcase />, label: "Trabalho" },
+    { id: 8, icon: <GraduationCap />, label: "Academico" },
+    { id: 9, icon: <Globe />, label: "Idiomas" },
+];
 
 
 const IconSelectionPage: React.FC = () => {
+    const [selectedIcon, setSelectedIcon] = useState<number | null>(null);
+
+    const handleSelect = (id: number) => {
+        setSelectedIcon(id);
+    };
+
     return (
         <div className="flex flex-col items-center justify-center">
-        <div className="mt-32 md:mt-40 flex flex-col justify-center items-center gap-4 md:gap-6 w-full max-w-md md:max-w-xl">
-            <SectionTitle>Escolha um Ícone</SectionTitle>
-            
-            <IconGrid />
+            <div className="mt-32 md:mt-40 flex flex-col justify-center items-center gap-4 md:gap-6 w-full max-w-md md:max-w-xl">
+                <SectionTitle>Escolha um Ícone</SectionTitle>
 
-            <div className="flex gap-3 w-full">
-                <Button full type="outlined">
-                    Voltar
-                </Button>
-                <Button full disabled>Próximo</Button>
+                <div className="grid grid-cols-3 gap-2 md:gap-3.5 w-fit items-center justify-center">
+                    {icons.map(({ id, icon, label }) => (
+                        <IconGridItem
+                            key={id}
+                            id={id}
+                            icon={icon}
+                            label={label}
+                            isSelected={selectedIcon === id}
+                            onClick={() => handleSelect(id)}
+                        />
+                    ))}
+                </div>
+
+                <div className="flex gap-3 w-full">
+                    <Button full type="outlined">
+                        Voltar
+                    </Button>
+                    <Button full disabled>Próximo</Button>
+                </div>
             </div>
-        </div>
         </div>
     );
 };
---
+++ src/components/IconGridItem/IconGrridItem.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+// TODO: ajustar hover do icone quando passar o mouse
+const IconGridItem = ({
+    icon,
+    id,
+    label,
+    isSelected,
+    onClick
+}: IIconGridItemProps) => {
+    const borderStyle = isSelected
+        ? "border-principal text-principal"
+        : "border-gray-300 text-gray-600";
+
+    const colorStyle = isSelected
+        ? "text-principal"
+        : "text-gray-700";
+
+    return (
+        <button
+            onClick={() => onClick(id)}
+            className={`flex flex-col items-center justify-center p-1.5 sm:p-3 rounded-lg shadow-md border bg-white ${borderStyle} hover:border-principal hover:text-principal transition`}
+        >
+            <div className={`w-6 h-6 sm:w-8 sm:h-8 md:w-10 md:h-10 ${colorStyle} hover:text-principal`}>{React.cloneElement(icon, { className: "w-full h-full" })}</div>
+            <span className={`mt-1 md:mt-2 text-xs md:text-sm font-semibold`}>{label}</span>
+        </button>
+    );
+}
+
+export default IconGridItem;
