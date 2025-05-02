def7ecdbdb9992c851f88625f571942a8136a5b6
julioceno <julioreplay@gmail.com>
2025-05-02 01:18:32 -0300

    feat: eslint disable in types.d.ts

--- src/context/toast/types.d.ts
+++ src/context/toast/types.d.ts
@@ -1,5 +1,6 @@
 import { IToastProps } from "@/components/Toast/types";
 
+// eslint-disable @typescript-eslint/no-empty-interface
 interface IShowToast extends Omit<IToastProps, 'onClose'> {}
 
 interface IToastContext {
