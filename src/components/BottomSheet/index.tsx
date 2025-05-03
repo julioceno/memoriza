66035b2453e8c7cdb90979680391041ee8baddce
julioceno <julioreplay@gmail.com>
2025-05-03 14:27:03 -0300

    feat: close bottom sheet on drag

--- src/components/BottomSheet/index.tsx
+++ src/components/BottomSheet/index.tsx
@@ -13,27 +13,34 @@ export const BottomSheet: React.FC<IBottomSheetProps> = ({
 
   return (
     <AnimatePresence>
-      {isOpen && (
+    {isOpen && (
+      <motion.div
+        className="fixed inset-0 flex items-end bg-black/20 z-50"
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        exit={{ opacity: 0 }}
+        transition={{ duration: 0.5 }}
+        onClick={handleBackdropClick}
+      >
         <motion.div
-          className="fixed inset-0 flex items-end bg-black/20 z-50"
-          initial={{ opacity: 0 }}
-          animate={{ opacity: 1 }}
-          exit={{ opacity: 0 }}
-          transition={{ duration: 0.5 }}
-          onClick={handleBackdropClick}
+          className="w-full bg-white rounded-t-3xl p-4 shadow-lg"
+          initial={{ y: "100%" }}
+          animate={{ y: 0 }}
+          exit={{ y: "100%" }}
+          transition={{ duration: 0.15 }}
+          drag="y"
+          dragConstraints={{ top: 0, bottom: 0 }} 
+          onDragEnd={(_, info) => {
+            if (info.offset.y > 100) {
+              onClose();
+            }
+          }}
         >
-          <motion.div 
-            className="w-full bg-white rounded-t-3xl p-4 shadow-lg"
-            initial={{ y: "100%" }}
-            animate={{ y: 0 }}
-            exit={{ y: "100%" }}
-            transition={{ duration: 0.3 }}
-          >
-            <div className="mt-1 w-12 h-1 bg-gray-700 mx-auto rounded"></div>
-            {children}
-          </motion.div>
+          <div className="mt-1 w-12 h-1 bg-gray-700 mx-auto rounded cursor-pointer"></div>
+          {children}
         </motion.div>
-      )}
-    </AnimatePresence>
+      </motion.div>
+    )}
+  </AnimatePresence>
   );
 };
