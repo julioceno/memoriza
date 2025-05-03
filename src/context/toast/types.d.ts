d0f4b99d05de38fed9cadcfe63f80e1439e7e6a2
julioceno <julioreplay@gmail.com>
2025-05-03 00:35:07 -0300

    chore: adjusting type

--- src/context/toast/types.d.ts
+++ src/context/toast/types.d.ts
@@ -1,7 +1,7 @@
 import { IToastProps } from "@/components/Toast/types";
 
-interface IShowToast extends Omit<IToastProps, 'onClose'> {}
+type IShowToast = Omit<IToastProps, 'onClose'>;
 
 interface IToastContext {
     show: (value: IShowToast) => void;
-};
+}
