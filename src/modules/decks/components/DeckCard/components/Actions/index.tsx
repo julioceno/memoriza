9f8d098a1f80571191d7107c0af36b569024fce1
julioceno <julioreplay@gmail.com>
2025-05-04 23:37:41 -0300

    feat: change icon edit in actions

--- src/modules/decks/components/DeckCard/components/Actions/index.tsx
+++ src/modules/decks/components/DeckCard/components/Actions/index.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Edit, GripVertical, Trash2 } from "lucide-react";
+import { GripVertical, Pencil, Trash2 } from "lucide-react";
 import DeleteDialog from "@/components/DeleteDialog";
 import { IDeckActionsProps } from "./types";
 import { useSortable } from "@dnd-kit/sortable";
@@ -16,7 +16,7 @@ const Actions: React.FC<IDeckActionsProps> = ({ id }) => {
     <>
       <div className="flex items-center gap-1.5 sm:gap-2">
         <button className="text-gray-500 hover:text-gray-900 cursor-pointer">
-          <Edit className="w-5 h-5" />
+          <Pencil className="w-5 h-5" />
         </button>
         <button
           className="text-gray-500 hover:text-gray-900 cursor-pointer"
