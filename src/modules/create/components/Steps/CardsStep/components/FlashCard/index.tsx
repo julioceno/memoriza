768730ff1b8363e525a5e1ba920d889bd241760b
julioceno <julioreplay@gmail.com>
2025-05-05 22:34:16 -0300

    feat: truncateText

--- src/modules/create/components/Steps/CardsStep/components/FlashCard/index.tsx
+++ src/modules/create/components/Steps/CardsStep/components/FlashCard/index.tsx
@@ -1,13 +1,14 @@
 import React from "react";
 import { Actions } from "./components";
+import { truncateText } from "@/utils";
 
 export const FlashCard: React.FC<IFlashCardProps> = ({ id, title, answer }) => {
   return (
-    <div className="flex flex-col p-4 items-end bg-white h-full rounded-lg shadow-md hover:shadow-lg transition">
+    <div className="flex flex-col p-4 bg-white h-full items-end sm:w-sm rounded-lg shadow-md hover:shadow-lg transition">
       <Actions id={id} />
       <div className="flex-start w-full">
-        <h3 className="text-gray-800 font-bold text-lg">{title}</h3>
-        <h3 className="text-gray-400 text-sm">{answer}</h3>
+        <h3 className="text-gray-800 font-bold text-lg">{truncateText(title, 40)}</h3>
+        <h3 className="text-gray-400 text-sm">{truncateText(answer, 30)}</h3>
       </div>
     </div>
   );
