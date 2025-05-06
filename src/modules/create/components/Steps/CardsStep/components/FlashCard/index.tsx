b9389d9f42804427014085bf8266fd9abc26ab0e
julioceno <julioreplay@gmail.com>
2025-05-05 23:05:26 -0300

    feat: improve createPage

--- src/modules/create/components/Steps/CardsStep/components/FlashCard/index.tsx
+++ src/modules/create/components/Steps/CardsStep/components/FlashCard/index.tsx
@@ -3,11 +3,11 @@ import { Actions } from "./components";
 
 export const FlashCard: React.FC<IFlashCardProps> = ({ id, title, answer }) => {
   return (
-    <div className="flex flex-col p-4 h-24 bg-white items-end sm:w-sm rounded-lg shadow-md hover:shadow-lg transition">
+    <div className="flex flex-col p-4 min-h-24 bg-white items-end sm:w-sm rounded-lg shadow-md hover:shadow-lg transition">
       <Actions id={id} />
       <div className="flex-start w-full">
-        <h3 className="text-gray-800 font-bold text-lg truncate">{title}</h3>
-        <h3 className="text-gray-400 text-sm truncate">{answer}</h3>
+        <h3 className="text-gray-800 font-bold text-sm sm:text-lg truncate">{title}</h3>
+        <h3 className="text-gray-400 text-xs sm:text-md truncate">{answer}</h3>
       </div>
     </div>
   );
--- src/modules/decks/hooks/index.ts
+++ src/modules/decks/hooks/index.ts
@@ -21,27 +21,6 @@ export function useDecks() {
         { id: "7", title: "Banco de Dados SQL", cardCount: 16, statusBadgeType: StatusBadgeEnum.SUCCESS },
         { id: "8", title: "Redes de Computadores", cardCount: 11, statusBadgeType: StatusBadgeEnum.WARNING },
         { id: "9", title: "Estrutura de Sistemas Operacionais", cardCount: 13, statusBadgeType: StatusBadgeEnum.SUCCESS },
-        { id: "10", title: "Introdução ao C++", cardCount: 10, statusBadgeType: StatusBadgeEnum.SUCCESS },
-        { id: "11", title: "Desenvolvimento Web com Node.js", cardCount: 17, statusBadgeType: StatusBadgeEnum.SUCCESS },
-        { id: "12", title: "Arquitetura de Software", cardCount: 19, statusBadgeType: StatusBadgeEnum.WARNING },
-        { id: "13", title: "Machine Learning com Python", cardCount: 25, statusBadgeType: StatusBadgeEnum.SUCCESS },
-        { id: "14", title: "Desenvolvimento de APIs RESTful", cardCount: 15, statusBadgeType: StatusBadgeEnum.SUCCESS },
-        { id: "15", title: "Introdução ao Kotlin", cardCount: 12, statusBadgeType: StatusBadgeEnum.ERROR },
-        { id: "16", title: "Programação Funcional em JavaScript", cardCount: 14, statusBadgeType: StatusBadgeEnum.WARNING },
-        { id: "17", title: "Desenvolvimento de Jogos com Unity", cardCount: 20, statusBadgeType: StatusBadgeEnum.SUCCESS },
-        { id: "18", title: "DevOps e Integração Contínua", cardCount: 16, statusBadgeType: StatusBadgeEnum.SUCCESS },
-        { id: "19", title: "Segurança em Redes", cardCount: 18, statusBadgeType: StatusBadgeEnum.ERROR },
-        { id: "20", title: "Flutter para Desenvolvimento Mobile", cardCount: 22, statusBadgeType: StatusBadgeEnum.WARNING },
-        { id: "21", title: "Testes Unitários em Java", cardCount: 11, statusBadgeType: StatusBadgeEnum.SUCCESS },
-        { id: "22", title: "Introdução ao Swift", cardCount: 13, statusBadgeType: StatusBadgeEnum.ERROR },
-        { id: "23", title: "Big Data e Hadoop", cardCount: 17, statusBadgeType: StatusBadgeEnum.SUCCESS },
-        { id: "24", title: "DevSecOps: Segurança no Ciclo de Vida do Desenvolvimento", cardCount: 19, statusBadgeType: StatusBadgeEnum.WARNING },
-        { id: "25", title: "NoSQL: MongoDB e Firebase", cardCount: 14, statusBadgeType: StatusBadgeEnum.SUCCESS },
-        { id: "26", title: "Blockchain e Criptomoedas", cardCount: 21, statusBadgeType: StatusBadgeEnum.SUCCESS },
-        { id: "27", title: "Automação de Testes com Selenium", cardCount: 16, statusBadgeType: StatusBadgeEnum.WARNING },
-        { id: "28", title: "Técnicas de Otimização de Código", cardCount: 12, statusBadgeType: StatusBadgeEnum.SUCCESS },
-        { id: "29", title: "Administração de Sistemas Linux", cardCount: 14, statusBadgeType: StatusBadgeEnum.ERROR },
-        { id: "30", title: "Inteligência Artificial e Redes Neurais", cardCount: 20, statusBadgeType: StatusBadgeEnum.SUCCESS }
     ]);
 
     return {
