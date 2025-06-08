5d345908a154c1f34e202e90aeb04f402d324804
julioceno <julioreplay@gmail.com>
2025-06-08 12:12:34 -0300

    feat: create controls for forms

--- src/modules/create/components/Steps/CardsStep/components/CreateFlashCardModal/index.tsx
+++ src/modules/create/components/Steps/CardsStep/components/CreateFlashCardModal/index.tsx
@@ -1,39 +1,44 @@
 import { BottomSheet, Input, Modal } from "@/components";
 import { ICreateFlashCardModal } from "./types";
 import { useIsMobile } from "@/hooks";
+import { useState } from "react";
 
 export function CreateFlashCardModal({
     isOpen,
     onClose,
-    
 }: ICreateFlashCardModal) {
     const isMobile = useIsMobile()
 
-    const Content = () => (
-        <>
-            <h2 className="text-black font-bold text-start text-md md:text-md md:text-lg">Criar novo flash card</h2>
-            <p className="text-black text-start text-sm md:text-base">É interessante que o flash card tenha perguntas e respostas claras, para que facilite os seus estudos :)</p>
+    const Content = () => {
+        const [question, setQuestion] = useState<string>("");
+        const [answer, setAnswer] = useState<string>("");
 
-            <div className="flex flex-col gap-2 mt-3">
-                <Input
-                    value=""
-                    placeholder="Pergunta"
-                    onChange={() => { }}
-                    textarea
-                    rows={4}
-                />
-                <Input
-                    value=""
-                    placeholder="Resposta"
-                    onChange={() => { }}
-                    textarea
-                    rows={4}
-                />
-            </div>
-        </>
-    )
+        return (
+            <>
+                <h2 className="text-black font-bold text-start text-md md:text-md md:text-lg">Criar novo flash card</h2>
+                <p className="text-black text-start text-sm md:text-base">É interessante que o flash card tenha perguntas e respostas claras, para que facilite os seus estudos :)</p>
 
-    if(isMobile) {
+                <div className="flex flex-col gap-2 mt-3 h-full">
+                    <Input
+                        value={question}
+                        placeholder="Pergunta"
+                        onChange={e => setQuestion(e.target.value)}
+                        textarea
+                        rows={4}
+                    />
+                    <Input
+                        value={answer}
+                        placeholder="Resposta"
+                        onChange={e => setAnswer(e.target.value)}
+                        textarea
+                        rows={4}
+                    />
+                </div>
+            </>
+        )
+    }
+
+    if (isMobile) {
         return (
             <BottomSheet
                 isOpen={isOpen}
